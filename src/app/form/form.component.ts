07319533cade0dbfb49ad4bc95ecb08aa8f139cb
Vartan Benohanian <vartanbeno@gmail.com>
2018-07-12 21:41:18 -0400

    polish form, timespans only appear when user checks top or controversial

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public sorts: Array<String> = ['hot', 'new', 'rising', 'controversial', 'top'];
+  public timespans: Array<String> = ['all', 'year', 'month', 'week', 'day', 'hour'];
+  public sortValue: String = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setSortValue(e: any) {
+    if (e.target.checked) {
+      this.sortValue = e.target.value;
+      console.log(this.sortValue);
+    }
   }
 
 }
