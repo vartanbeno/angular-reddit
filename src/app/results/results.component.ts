4854912073189a35d5ccd63c75dddc1d6b5cad6b
vartanbeno <vartanbeno@gmail.com>
2018-07-13 15:30:46 +0100

    add sort options on results page

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ResultsComponent implements OnInit {
 
   public date: Date;
+  public sortBy: Array<String> = ['default','newest', 'oldest', 'most comments', 'most upvotes'];
 
   constructor() { }
 
